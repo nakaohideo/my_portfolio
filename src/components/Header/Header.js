6bfee940debe317a7152194470c62938fd16bef7
Sakuraikin <kinkode0407@gmail.com>
2023-11-15 15:01:21 -0800

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Nav, Logo, NavLink, Bars, NavMenu, NavBtn } from "./HeaderElements";
+
+const Header = ({ toggle }) => {
+  return (
+    <div className="Container" style={{padding: 0}}>
+      <Nav>
+        <Logo to="/">
+          <img
+            src="/logo.png"
+            alt="logo"
+          />
+        </Logo>
+        <NavMenu>
+          <NavLink className="menu-item" to="projects">
+            Projects
+          </NavLink>
+          <NavLink className="menu-item" to="about">
+            About
+          </NavLink>
+          <NavLink className="menu-item" to="contact">
+            Contact
+          </NavLink>
+        </NavMenu>
+        <NavBtn>
+          <a
+            className="btn PrimaryBtn"
+            href="https://docs.google.com/document/d/1rXT-eZJugnJsZWOGL6k8oQQy6hzWhlOPAxun-Cq2GXo/edit"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Resume
+          </a>
+        </NavBtn>
+        <Bars onClick={toggle} />
+      </Nav>
+    </div>
+  );
+};
+
+export default Header;
